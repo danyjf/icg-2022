480daaf20e284b0dcf6394d3fc6a3474369e3c00
Daniel Francisco <deny.jo@hotmail.com>
2022-04-30 16:15:22 +0100

    Add empty scene with torus

---
+++ projeto-1/torus-forest/helper.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const helper = {
+
+    initEmptyScene: function (sceneElements) {
+
+        // ************************** //
+        // Create the 3D scene
+        // ************************** //
+        sceneElements.sceneGraph = new THREE.Scene();
+
+
+        // ************************** //
+        // Add camera
+        // ************************** //
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 500);
+        sceneElements.camera = camera;
+        camera.position.set(0, 30, 50);
+        camera.lookAt(0, 0, 0);
+
+
+        // ************************** //
+        // Illumination
+        // ************************** //
+
+        // ************************** //
+        // Add ambient light
+        // ************************** //
+        const ambientLight = new THREE.AmbientLight('rgb(255, 255, 255)', 0.2);
+        sceneElements.sceneGraph.add(ambientLight);
+
+        // ***************************** //
+        // Add spotlight (with shadows)
+        // ***************************** //
+        const spotLight = new THREE.SpotLight('rgb(255, 255, 255)', 0.8);
+        spotLight.position.set(-5, 8, 0);
+        sceneElements.sceneGraph.add(spotLight);
+
+        // Setup shadow properties for the spotlight
+        spotLight.castShadow = true;
+        spotLight.shadow.mapSize.width = 2048;
+        spotLight.shadow.mapSize.height = 2048;
+
+        // *********************************** //
+        // Create renderer (with shadow map)
+        // *********************************** //
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
+        sceneElements.renderer = renderer;
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setClearColor('rgb(255, 255, 255)', 1.0);
+        renderer.setSize(width, height);
+
+        // Setup shadowMap property
+        renderer.shadowMap.enabled = true;
+        renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+
+        // **************************************** //
+        // Add the rendered image in the HTML DOM
+        // **************************************** //
+        const htmlElement = document.querySelector("#tagTorusForest");
+        htmlElement.appendChild(renderer.domElement);
+    },
+
+    render: function render(sceneElements) {
+        sceneElements.renderer.render(sceneElements.sceneGraph, sceneElements.camera);
+    },
+};
---
+++ projeto-1/torus-forest/scene.js
@@ -0,0 +1,94 @@
+"use strict";
+
+// To store the scene graph, and elements usefull to rendering the scene
+const sceneElements = {
+    sceneGraph: null,
+    camera: null,
+    renderer: null,
+};
+
+
+// Functions are called
+//  1. Initialize the empty scene
+//  2. Add elements within the scene
+//  3. Render the scene
+helper.initEmptyScene(sceneElements);
+load3DObjects(sceneElements.sceneGraph);
+requestAnimationFrame(computeFrame);
+
+// HANDLING EVENTS
+// Event Listeners
+
+window.addEventListener('resize', resizeWindow);
+
+//To keep track of the keyboard - WASD
+var keyD = false, keyA = false, keyS = false, keyW = false;
+document.addEventListener('keydown', onDocumentKeyDown, false);
+document.addEventListener('keyup', onDocumentKeyUp, false);
+
+// Update render image size and camera aspect when the window is resized
+function resizeWindow(eventParam) {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    sceneElements.camera.aspect = width / height;
+    sceneElements.camera.updateProjectionMatrix();
+
+    sceneElements.renderer.setSize(width, height);
+}
+
+function onDocumentKeyDown(event) {
+    switch (event.keyCode) {
+        case 68: //d
+            keyD = true;
+            break;
+        case 83: //s
+            keyS = true;
+            break;
+        case 65: //a
+            keyA = true;
+            break;
+        case 87: //w
+            keyW = true;
+            break;
+    }
+}
+function onDocumentKeyUp(event) {
+    switch (event.keyCode) {
+        case 68: //d
+            keyD = false;
+            break;
+        case 83: //s
+            keyS = false;
+            break;
+        case 65: //a
+            keyA = false;
+            break;
+        case 87: //w
+            keyW = false;
+            break;
+    }
+}
+
+// Create and insert in the scene graph the models of the 3D scene
+function load3DObjects(sceneGraph) {
+    // Create torus
+    const torusGeometry = new THREE.TorusGeometry(10, 3, 16, 100);
+    const torusMaterial = new THREE.MeshPhongMaterial({color: 0xffff00});
+    const torusObject = new THREE.Mesh(torusGeometry, torusMaterial);
+    torusObject.rotation.x = Math.PI / 2;
+    sceneGraph.add(torusObject);
+
+    const torusWireframeMaterial = new THREE.MeshBasicMaterial({color: 0x000000, wireframe: true});
+    const torusWireframeObject = new THREE.Mesh(torusGeometry, torusWireframeMaterial);
+    torusWireframeObject.rotation.x = torusObject.rotation.x;
+    sceneGraph.add(torusWireframeObject);
+}
+
+function computeFrame(time) {
+    // Rendering
+    helper.render(sceneElements);
+
+    // Call for the next frame
+    requestAnimationFrame(computeFrame);
+}
